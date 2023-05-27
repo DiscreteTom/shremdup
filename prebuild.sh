7588d5610c8e2101757b83d279cb9ca034c4c7be
DiscreteTom <discrete_tom@outlook.com>
2023-05-27 12:32:17 +0800

    ci: fix protoc version

--- prebuild.sh
+++ prebuild.sh
@@ -3,11 +3,7 @@
 # this file is used in github actions to install the dependencies
 # according to https://github.com/rust-build/rust-build.action/tree/master
 
-wget https://github.com/protocolbuffers/protobuf/releases/download/v3.20.3/protoc-3.20.3-linux-x86_64.zip
-unzip protoc-3.20.3-linux-x86_64.zip -d protoc3
+wget https://github.com/protocolbuffers/protobuf/releases/download/v23.2/protoc-23.2-linux-x86_64.zip
+unzip protoc-23.2-linux-x86_64.zip -d protoc3
 mv protoc3/bin/* /usr/local/bin/
-export PROTOC=/usr/local/bin/protoc
-export RUST_LOG=debug
-
-# test the build
-RUST_LOG=debug cargo build
+mv protoc3/include/* /usr/local/include/
